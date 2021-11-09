98701d61fcb99504db5b06f781e0ba71dc723b40
natinusala <natinusala@gmail.com>
2021-11-09 20:37:28 +0100

    Some more Image tests, 2247 more to go

--- Tests/LoftwingTests/Views/ImageSpec.swift
+++ Tests/LoftwingTests/Views/ImageSpec.swift
@@ -67,6 +67,8 @@ class ImageSpec: QuickSpec {
                 }
             }
 
+            // TODO: test with portrait and square views too
+
             context("when laying out") {
                 let stretchParams: [(width: Float, height: Float)] = [
                     (width: 0, height: 0),
@@ -92,9 +94,9 @@ class ImageSpec: QuickSpec {
                 let fitParams: [(width: Float, height: Float, expected: Rect)] = [
                     // Square upscale
                     (width: 200, height: 200, expected: Rect(x: 100, y: 0, width: 600, height: 600)),
-                    // Same res as image
+                    // Same res as view
                     (width: 800, height: 600, expected: Rect(x: 0, y: 0, width: 800, height: 600)),
-                    // Same ratio as image
+                    // Same ratio as view
                     (width: 600, height: 450, expected: Rect(x: 0, y: 0, width: 800, height: 600)),
                     // Landscape upscale
                     (width: 600, height: 225, expected: Rect(x: 0, y: 150, width: 800, height: 300)),
@@ -103,13 +105,86 @@ class ImageSpec: QuickSpec {
                 ]
                 fitParams.forEach { (width, height, expected) in
                     context("when fitting an image of size \(width)x\(height)") {
-                        it("makes an imageRect of \(expected)") {
+                        it("fits the image in \(expected)") {
                             let source = ImageSourceMock(width: width, height: height)
                             let image = ImageMock(source: source)
 
                             image.width(800.dip)
                             image.height(600.dip)
                             image.scalingMode(.fit)
+
+                            image.layout()
+
+                            expect(image.imageRect).to(equal(expected))
+                        }
+                    }
+                }
+
+                let centerParams: [(width: Float, height: Float, expected: Rect)] = [
+                    // Same dimensions as view
+                    (width: 800, height: 600, expected: Rect(x: 0, y: 0, width: 800, height: 600)),
+                    // Square
+                    (width: 200, height: 200, expected: Rect(x: 300, y: 200, width: 200, height: 200)),
+                    // Same ratio as view
+                    (width: 600, height: 450, expected: Rect(x: 100, y: 75, width: 600, height: 450)),
+                    // Bigger image
+                    (width: 1280, height: 720, expected: Rect(x: -240, y: -60, width: 1280, height: 720)),
+                    // Landscape
+                    (width: 600, height: 225, expected: Rect(x: 100, y: 187.5, width: 600, height: 225)),
+                    // Portrait
+                    (width: 225, height: 400, expected: Rect(x: 287.5, y: 100, width: 225, height: 400)),
+                ]
+                centerParams.forEach { (width, height, expected) in
+                    context("when centering an image of \(width)x\(height)") {
+                        it("centers the image in \(expected)") {
+                            let source = ImageSourceMock(width: width, height: height)
+                            let image = ImageMock(source: source)
+
+                            image.width(800.dip)
+                            image.height(600.dip)
+                            image.scalingMode(.center)
+
+                            image.layout()
+
+                            expect(image.imageRect).to(equal(expected))
+                        }
+                    }
+                }
+
+                let integerParams: [(width: Float, height: Float, scaleFactor: Int?, expected: Rect)] = [
+                    // Scale as much as possible - same dimensions as view
+                    (width: 800, height: 600, scaleFactor: nil, expected: Rect(x: 0, y: 0, width: 800, height: 600)),
+                    // Scale as much as possible - square
+                    (width: 200, height: 200, scaleFactor: nil, expected: Rect(x: 100, y: 0, width: 600, height: 600)),
+                    // Scale as much as possible - landscape
+                    (width: 150, height: 75, scaleFactor: nil, expected: Rect(x: 25, y: 112.5, width: 750, height: 375)),
+                    // Scale as much as possible - portrait
+                    (width: 75, height: 150, scaleFactor: nil, expected: Rect(x: 250, y: 0, width: 300, height: 600)),
+
+                    // 1:1 scaling - same dimensions as view
+                    // 1:1 scaling - square
+                    // 1:1 scaling - landscape
+                    // 1:1 scaling - portrait
+
+                    // Fixed scaling - same dimensions as view
+                    // Fixed scaling - square
+                    // Fixed scaling - landscape
+                    // Fixed scaling - portrait
+
+                    // Larger scaling - same dimensions as view
+                    // Larger scaling - square
+                    // Larger scaling - landscape
+                    // Larger scaling - portrait
+                ]
+                integerParams.forEach { (width, height, scaleFactor, expected) in
+                    context("when scaling by \(String(describing: scaleFactor)) an image of \(width)x\(height)") {
+                        it("scales the image in \(expected)") {
+                            let source = ImageSourceMock(width: width, height: height)
+                            let image = ImageMock(source: source)
+
+                            image.width(800.dip)
+                            image.height(600.dip)
+                            image.scalingMode(.integer(scaleFactor))
 
                             image.layout()
 
